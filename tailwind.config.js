6cfab8e0d70097b730ee44a59c1f518b519446d3
Van Tran <van.tran@asnet.com.vn>
2022-08-05 11:06:19 +0700

    Define theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      theme: {
+        color: {
+          default: '#FFF',
+          highlight: '#FF0079',
+          'light-gray': '#F8F8F8',
+          dark: '#494C62'
+        }
+      }
+    },
   },
   plugins: [],
 };
