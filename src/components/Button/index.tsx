73f520298e7e7cff1f485fb9e914521046daa64a
Van Tran <van.tran@asnet.com.vn>
2022-08-05 14:23:50 +0700

    Implement Button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,36 @@
+// Libraries
+import { FC, ReactNode } from 'react';
+
+// Types
+import { ButtonVariants } from '@common-types/button';
+
+interface ButtonProps {
+  children: ReactNode;
+  variant?: ButtonVariants;
+  isDisabled?: boolean;
+  onClick: () => void;
+}
+
+const Button: FC<ButtonProps> = ({
+  children,
+  variant = ButtonVariants.default,
+  isDisabled = false,
+  onClick,
+}) => (
+  <button
+    disabled={isDisabled}
+    className={[
+      'px-5 py-1 leading-8 text-default rounded uppercase',
+      `${
+        variant === ButtonVariants.default
+          ? ' bg-highlight'
+          : ' border-2 border-default'
+      }`,
+    ].join('')}
+    onClick={onClick}
+  >
+    {children}
+  </button>
+);
+
+export default Button;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {
-      theme: {
-        color: {
-          default: '#FFF',
-          highlight: '#FF0079',
-          'light-gray': '#F8F8F8',
-          dark: '#494C62'
-        }
-      }
+    color: {
+      'light-gray': '#F8F8F8',
+      dark: '#494C62'
     },
+    extend: {
+      colors: {
+        default: '#FFF',
+        highlight: '#FF0079',
+        'light-gray': '#F8F8F8',
+        dark: '#494C62'
+      }
+    }
   },
   plugins: [],
 };
